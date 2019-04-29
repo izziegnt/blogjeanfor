d78a95b182524bab4d010b07a43a89c2151969a1
Izzie Geniet <48997027+izziegnt@users.noreply.github.com>
2019-04-29 15:21:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,35 +13,8 @@ spl_autoload_register(function ($class) {
 });
 
     $array = explode("/", filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL, FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_LOW));
-    
-   // secure all Post data 
-// comment post data
-if(isset($_POST['submit']))
-{
-$author = $_POST['author'];
-$author = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_STRING);
-
-$comment = $_POST['comment'];
-$comment = filter_input(INPUT_POST, 'comment', FILTER_SANITIZE_STRING);
-
-$post_id = $_POST['post_id'];
-$post_id = filter_input(INPUT_POST, 'post_id', FILTER_SANITIZE_NUMBER_INT); 
-}
-
-// admin post data
-if(isset($_POST['submit']))
-{
-$title = $_POST['title'];
-$title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
-
-$content = $_POST['content'];
-$content = filter_input(INPUT_POST, 'content', FILTER_SANITIZE_STRING);
-
-$id = $_POST['id'];
-$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
-}
-
-$url = array_slice($array, 2);
+  
+$url = array_slice($array, 2); 
     
 try {
     if (isset($url[0]))
