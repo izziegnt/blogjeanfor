f239921c6cbb4a3a988e06b390f0fa1b23df682c
Izzie Geniet <48997027+izziegnt@users.noreply.github.com>
2019-04-29 15:23:41 +0200

    Update CommentController.php

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -6,22 +6,29 @@ class CommentController
     public function addComment()
   	{			
 		$errors = [];
-
-		if(!isset($_POST['author']) || empty($_POST['comment']) || !isset($_POST['comment']) || empty($_POST['comment'])) { 
+     
+       $safeInput = filter_input_array(INPUT_POST, [ 
+       'author'  => FILTER_SANITIZE_STRING,
+       'comment' => FILTER_SANITIZE_STRING,
+       'post_id' => FILTER_SANITIZE_NUMBER_INT
+       ]);
+        
+        
+		if(is_null($safeInput['author']) || is_null($safeInput['comment']) || empty($safeInput['author']) || empty($safeInput['comment'])) { 
 			$errors = "Veuillez remplir tous les champs !";}
 
-		if(!preg_match('`^[[:alnum:]]{3,15}$`',$_POST['author'])) {
+		if(!preg_match('`^[[:alnum:]]{3,15}$`',$safeInput['author'])) {
 			$errors = "Pseudo incorrect";
 			return $errors;
 		}
 
-		if(!isset($_POST['post_id']) || empty($_POST['post_id'])) { 
+		if(is_null($safeInput['post_id'])) { 
 			$errors = "Il n'y a pas d'articles pour ce commentaire";
 		}
 
 		if(empty($errors)) {
 			$commentManager = new CommentManager();
-			$commentManager->addComment($_POST['author'], $_POST['comment'], $_POST['post_id']);
+			$commentManager->addComment($safeInput['author'], $safeInput['comment'], $safeInput['post_id']);
   		}
   	}
         
